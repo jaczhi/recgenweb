1eac5e0844496cf6e06e2455362ddf72321ff03f
quickTwix898 <jacobazhi@gmail.com>
2019-11-13 10:50:40 -0800

    Worked on javascript generator

--- javascript/generator.js
+++ javascript/generator.js
@@ -28,6 +28,14 @@ var programs = ["", "university", "program", "employment"];
 var pronouns = ["he", "she", "they"];
 var proref = ["him", "her", "them"];
 
+var traitArr = [false, false, false, false, false, false, false];
+var traitNames = ["Kind", "Detail-oriented", "Responsible", "Team Player", "Quick-thinking", "Works Under Pressure", "OTHER"];
+var otherTrait = "";
+var traitEmph = [-1, -1, -1, -1, -1, -1, -1];
+
+var teacherName = "";
+var title = 0;
+var titleArr = ["", "Dr. ", "Mr. ", "Mrs. ", "Ms."];
 function switchPage(page) {
 	if(page==0) {
 		document.getElementById("javascript-generator").innerHTML = "" + 
@@ -110,11 +118,80 @@ function switchPage(page) {
                         keepGoing = false;
                   }
 		}
-		if(keepGoing) {document.getElementById("javascript-generator").innerHTML = "<h3>How do you feel about " + 
+		if(keepGoing) {
+		//traitArr = [false, false, false, false, false, false, false];
+		document.getElementById("javascript-generator").innerHTML = "<h3>How do you feel about " + 
 		firstName + "?</h3>" + 
 		"<p>What traits do you see in them?</p>" + 
-		"<input type=\"checkbox\" name=\"traits\" value=\"0\">Kind<br>" + 
-		"<input type=\"checkbox\" name=\"traits\" value=\"1\">Detail-oriented<br>";}
+		"<input type=\"checkbox\" name=\"traits\" value=\"0\" onclick=\"page6traits(0)\">Kind<br>" + 
+		"<input type=\"checkbox\" name=\"traits\" value=\"1\" onclick=\"page6traits(1)\">Detail-oriented<br>" + 
+		"<input type=\"checkbox\" name=\"traits\" value=\"2\" onclick=\"page6traits(2)\">Responsible<br>" + 
+		"<input type=\"checkbox\" name=\"traits\" value=\"3\" onclick=\"page6traits(3)\">Team Player<br>" + 
+		"<input type=\"checkbox\" name=\"traits\" value=\"4\" onclick=\"page6traits(4)\">Quick-Thinking<br>" + 
+		"<input type=\"checkbox\" name=\"traits\" value=\"5\" onclick=\"page6traits(5)\">Works Under Pressure<br>" + 
+		"<input type=\"checkbox\" name=\"traits\" value=\"6\" onclick=\"page6traits(6)\">Other<br>";}
+	} if(page==7) {
+		var override = false;
+		keepGoing = true;
+                try {otherTrait = document.getElementById("extraTrait").value;}
+		catch(e_var_0) {override = true;}
+                console.log("name: " + studentName);
+                if(otherTrait === "" && !override) {
+			prevPage();
+                        console.log("Executed prevpage: no valid trait");
+                        keepGoing = false;
+                }
+
+		if(keepGoing) { 
+		document.getElementById("javascript-generator").innerHTML = "<h3>How do you feel about " + firstName + "?</h3>" +
+		"<p>Please select how much you want to mention these traits:</p>" +
+		"";
+		var result = "";
+		for(var i=0; i<traitArr.length; i++) {
+			if(traitArr[i]) {
+				var traitName=""
+				if(i==6){traitName=otherTrait;}
+				else{traitName=traitNames[i];}
+				result += "<h4>" + traitName + "</h4>";
+				result += "Less <input type=\"range\" min=\"1\" max=\"3\" value=\"2\" class=\"slider\" id=\"traitRange" + i + "\"> More";
+			}
+		}
+		document.getElementById("javascript-generator").innerHTML += result;}
+	} if(page==8) {
+		for(var i=0; i<traitArr.length; i++) {
+			if(document.getElementById("traitRange" + i) != null) {
+				traitEmph[i] = document.getElementById("traitRange" + i).value;
+				console.log(i + ": " + traitEmph[i]);
+			}
+		}
+		document.getElementById("javascript-generator").innerHTML = "" +
+		"<h3>Let's get to know you:</h3>" +
+		"<p>What is your name?</p>" + 
+		"<input type=\"text\" name=\"inputbox\"  placeholder=\"eg. Jane Doe\"     id=\"teachName\">";
+	} if(page==9) {
+		keepGoing = true;
+		try {teacherName = document.getElementById("teachName").value;} catch(e_var_60) {}
+		if(teacherName === "") {
+			prevPage();
+			console.log("Executed prevpage, no valid teacher name");
+			keepGoing = false;
+		}
+		if(keepGoing) {
+			document.getElementById("javascript-generator").innerHTML = "" + 
+			"<h3>Let's get to know you:</h3>" + 
+			"<p>What is your preferred title?</p>" + 
+			"<select>" + 
+			"<option value=\"none\" onclick=\"page9title(0)\">None</option>" + 
+			"<option value=\"dr\" onclick=\"page9title(1)\">Dr.</option>" + 
+			"<option value=\"mr\" onclick=\"page9title(2)\">Mr.</option>"+
+			"<option value=\"mrs\" onclick=\"page9title(3)\">Mrs.</option>" +
+			"<option value=\"ms\" onclick=\"page9title(4)\">Ms.</option>" + 
+			"</select>";
+		}
+	} if(page==10) {
+		document.getElementById("javascript-generator").innerHTML = "<h3>Customization</h3>" + 
+		"<p>Please choose your writer</p>" + 
+		"<p>Please be patient. Work in progress</p>"
 	}
 
 }
@@ -136,3 +213,22 @@ function page2yes(){
 }
 function page3sex(x) { gender = x; }
 function page4inst(x) { programType = x; }
+toggle6 = true;
+var temp;
+function page6traits(x) {
+	if(x==6 && toggle6) {
+	temp = document.getElementById("javascript-generator").innerHTML;
+	document.getElementById("javascript-generator").innerHTML += "<br><p>Add a trait...<p><input type=\"text\" name=\"traitbox\" placeholder=\"eg. smart\" id=\"extraTrait\">";
+	toggle6=!toggle6;
+	}else if(x==6 && !toggle6) {
+	document.getElementById("javascript-generator").innerHTML = temp;
+	toggle6=!toggle6;
+	}
+	traitArr[x] = !traitArr[x]; 
+	console.log("trait toggled to " + traitArr[x]);
+ }
+ function page9title(x) {
+ 	title = x;
+	console.log(x + ": " + titleArr[x]);
+ }
+ 	
